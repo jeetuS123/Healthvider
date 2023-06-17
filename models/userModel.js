c0a9bfb4f532a7c01f5786658eb3d29adf2a7e1b
jeetuS123 <116827843+jeetuS123@users.noreply.github.com>
2023-06-17 05:35:22 -0400

    Create userModel.js

---
+++ models/userModel.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      require: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      require: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    isDoctor: {
+      type: Boolean,
+      default: false,
+    },
+    notification: {
+      type: Array,
+      default: [],
+    },
+    seennotification: {
+      type: Array,
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
+
+const userModel = mongoose.model("users", userSchema);
+module.exports = userModel;
