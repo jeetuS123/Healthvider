e4127d316060577d71ee9977e8e50cea1f6174e9
shivam gupta <97395064+Apnacoding@users.noreply.github.com>
2023-06-17 04:36:08 -0400

    Create Appointments.js

---
+++ client/src/pages/Appointments.js
@@ -0,0 +1,77 @@
+import React, { useState, useEffect } from "react";
+import Layout from "../components/Layout";
+import axios from "axios";
+
+import { Table, message } from "antd";
+import moment from "moment";
+
+const Appointments = () => {
+  const [appointments, setAppointments] = useState();
+
+  const getAppointments = async () => {
+    try {
+      const res = await axios.get(
+        "/api/v1/user/user-appointment",
+
+        {
+          headers: {
+            Authorization: "Bearer " + localStorage.getItem("token"),
+          },
+        }
+      );
+      if (res.data.success) {
+        setAppointments(res.data.data);
+        // message.success(res.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getAppointments();
+  }, []);
+
+  const columns = [
+    {
+      title: "ID",
+      dataIndex: "_id",
+    },
+    // {
+    //   title: "Name",
+    //   dataIndex: "name",
+    //   render: (text, record) => (
+    //     <span>
+    //       {record.doctorId.firstName} {record.doctorId.lastName}
+    //     </span>
+    //   ),
+    // },
+    // {
+    //   title: "Phone",
+    //   dataIndex: "phone",
+    //   render: (text, record) => <span>{record.doctorId.phone}</span>,
+    // },
+    {
+      title: "Date & Time",
+      dataIndex: "date",
+      render: (text, record) => (
+        <span>
+          {moment(record.date).format("DD-MM-YYYY")} &nbsp;
+          {moment(record.time).format("HH:mm")}
+        </span>
+      ),
+    },
+    {
+      title: "Status",
+      dataIndex: "status",
+    },
+  ];
+
+  return (
+    <Layout>
+      <h1>Appointments</h1>
+      <Table columns={columns} dataSource={appointments} />
+    </Layout>
+  );
+};
+
+export default Appointments;
