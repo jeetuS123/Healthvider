72cc2a4808480d9b0c4b5fdf7c9be4569762ab7d
Jitendra Kumar <jitendrakumar@Jitendras-MacBook-Air.local>
2024-11-04 17:18:43 +0530

    updating db file

--- config/db.js
+++ config/db.js
@@ -3,9 +3,6 @@ const colors = require("colors");
 const ConnectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URL);
-    console.log(
-      `Conneted To Mongodb Databse ${conn.connection.host}`.bgMagenta.white
-    );
   } catch (error) {
     console.log(`error in mongodb ${error}`.bgRed.white);
   }
